0a60c1207f2be66ce41affd2e4ab3cff3535749e
Peter C <PeterChauYEG@users.noreply.github.com>
2016-08-13 21:04:36 -0600

    Enable Redux Devtools
    Added the Redux Devtools enhancer for a better developer experience

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,6 +1,11 @@
 import rootReducer from '../reducers';
-import {createStore} from 'redux';
+import {createStore, compose} from 'redux';
+
+// enable redux devtools... can this be done with Webpack instead?
+const enhancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)
 
 export default (initialState) => {
-  return createStore(rootReducer, initialState);
+  return createStore(rootReducer, initialState, enhancers);
 };
